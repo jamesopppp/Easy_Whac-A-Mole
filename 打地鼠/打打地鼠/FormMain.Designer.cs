2876fd34f3fbb8dfaae46f8323087120e6363cae
詹子贤 <jamessupermail@163.com>
2017-06-01 11:36:55 +0800

    Whac-A-Mole
    Easy Version

---
+++ 打地鼠/打打地鼠/FormMain.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace 打地鼠
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏难度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.简单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.中等ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.困难ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于打地鼠ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label0 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.游戏ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(975, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 游戏ToolStripMenuItem
+            // 
+            this.游戏ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.游戏难度ToolStripMenuItem,
+            this.简单ToolStripMenuItem,
+            this.中等ToolStripMenuItem,
+            this.困难ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.游戏ToolStripMenuItem.Name = "游戏ToolStripMenuItem";
+            this.游戏ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.游戏ToolStripMenuItem.Text = "&游戏";
+            // 
+            // 游戏难度ToolStripMenuItem
+            // 
+            this.游戏难度ToolStripMenuItem.Name = "游戏难度ToolStripMenuItem";
+            this.游戏难度ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.游戏难度ToolStripMenuItem.Text = "&游戏难度";
+            // 
+            // 简单ToolStripMenuItem
+            // 
+            this.简单ToolStripMenuItem.Checked = true;
+            this.简单ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.简单ToolStripMenuItem.Name = "简单ToolStripMenuItem";
+            this.简单ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.简单ToolStripMenuItem.Text = "简单";
+            this.简单ToolStripMenuItem.Click += new System.EventHandler(this.简单ToolStripMenuItem_Click);
+            // 
+            // 中等ToolStripMenuItem
+            // 
+            this.中等ToolStripMenuItem.Name = "中等ToolStripMenuItem";
+            this.中等ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.中等ToolStripMenuItem.Text = "中等";
+            this.中等ToolStripMenuItem.Click += new System.EventHandler(this.中等ToolStripMenuItem_Click);
+            // 
+            // 困难ToolStripMenuItem
+            // 
+            this.困难ToolStripMenuItem.Name = "困难ToolStripMenuItem";
+            this.困难ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.困难ToolStripMenuItem.Text = "困难";
+            this.困难ToolStripMenuItem.Click += new System.EventHandler(this.困难ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "&退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.帮助ToolStripMenuItem1,
+            this.关于打地鼠ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "&其它";
+            // 
+            // 帮助ToolStripMenuItem1
+            // 
+            this.帮助ToolStripMenuItem1.Name = "帮助ToolStripMenuItem1";
+            this.帮助ToolStripMenuItem1.Size = new System.Drawing.Size(136, 22);
+            this.帮助ToolStripMenuItem1.Text = "&帮助";
+            this.帮助ToolStripMenuItem1.Click += new System.EventHandler(this.帮助ToolStripMenuItem1_Click);
+            // 
+            // 关于打地鼠ToolStripMenuItem
+            // 
+            this.关于打地鼠ToolStripMenuItem.Name = "关于打地鼠ToolStripMenuItem";
+            this.关于打地鼠ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.关于打地鼠ToolStripMenuItem.Text = "&关于打地鼠";
+            this.关于打地鼠ToolStripMenuItem.Click += new System.EventHandler(this.关于打地鼠ToolStripMenuItem_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "地鼠左.jpg");
+            this.imageList1.Images.SetKeyName(1, "晕地鼠左.jpg");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "地鼠中.jpg");
+            this.imageList2.Images.SetKeyName(1, "晕地鼠中.jpg");
+            // 
+            // imageList3
+            // 
+            this.imageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList3.ImageStream")));
+            this.imageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList3.Images.SetKeyName(0, "地鼠右.jpg");
+            this.imageList3.Images.SetKeyName(1, "晕地鼠右.jpg");
+            // 
+            // label1
+            // 
+            this.label1.ImageIndex = 0;
+            this.label1.ImageList = this.imageList1;
+            this.label1.Location = new System.Drawing.Point(25, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 137);
+            this.label1.TabIndex = 1;
+            this.label1.Visible = false;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.ImageIndex = 0;
+            this.label2.ImageList = this.imageList1;
+            this.label2.Location = new System.Drawing.Point(25, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 137);
+            this.label2.TabIndex = 2;
+            this.label2.Visible = false;
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.ImageIndex = 0;
+            this.label3.ImageList = this.imageList1;
+            this.label3.Location = new System.Drawing.Point(499, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 137);
+            this.label3.TabIndex = 3;
+            this.label3.Visible = false;
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.ImageIndex = 0;
+            this.label4.ImageList = this.imageList1;
+            this.label4.Location = new System.Drawing.Point(499, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 137);
+            this.label4.TabIndex = 4;
+            this.label4.Visible = false;
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.ImageIndex = 0;
+            this.label5.ImageList = this.imageList2;
+            this.label5.Location = new System.Drawing.Point(185, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 103);
+            this.label5.TabIndex = 5;
+            this.label5.Visible = false;
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label6
+            // 
+            this.label6.ImageIndex = 0;
+            this.label6.ImageList = this.imageList2;
+            this.label6.Location = new System.Drawing.Point(185, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 103);
+            this.label6.TabIndex = 6;
+            this.label6.Visible = false;
+            this.label6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label7
+            // 
+            this.label7.ImageIndex = 0;
+            this.label7.ImageList = this.imageList2;
+            this.label7.Location = new System.Drawing.Point(657, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 103);
+            this.label7.TabIndex = 7;
+            this.label7.Visible = false;
+            this.label7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label8
+            // 
+            this.label8.ImageIndex = 0;
+            this.label8.ImageList = this.imageList2;
+            this.label8.Location = new System.Drawing.Point(657, 326);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 103);
+            this.label8.TabIndex = 8;
+            this.label8.Visible = false;
+            this.label8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label9
+            // 
+            this.label9.ImageIndex = 0;
+            this.label9.ImageList = this.imageList3;
+            this.label9.Location = new System.Drawing.Point(336, 48);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 137);
+            this.label9.TabIndex = 9;
+            this.label9.Visible = false;
+            this.label9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // label0
+            // 
+            this.label0.ImageIndex = 0;
+            this.label0.ImageList = this.imageList3;
+            this.label0.Location = new System.Drawing.Point(336, 302);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(111, 137);
+            this.label0.TabIndex = 10;
+            this.label0.Visible = false;
+            this.label0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label8_MouseDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 60000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(837, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.Location = new System.Drawing.Point(894, 305);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 46);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "分";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(825, 305);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(63, 46);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(975, 520);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "打地鼠";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem 游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏难度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 简单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 中等ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 困难ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 关于打地鼠ToolStripMenuItem;
+    }
+}
+
---
+++ 打地鼠/打打地鼠/FormMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 打地鼠
+{
+    public partial class FormMain : Form
+    {    
+        int suijishu = -1;
+        int n = 0;
+        bool dianji = true;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label8_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (dianji != true)
+            {
+                return;
+            }
+            int i = (Convert.ToInt32((sender as Label).Name.Substring(5, 1)));
+            switch (i)
+            {
+                case 1:
+                    label1.ImageIndex = 1;
+                    break;
+                case 2:
+                    label2.ImageIndex = 1;
+                    break;
+                case 3:
+                    label3.ImageIndex = 1;
+                    break;
+                case 4:
+                    label4.ImageIndex = 1;
+                    break;
+                case 5:
+                    label5.ImageIndex = 1;
+                    break;
+                case 6:
+                    label6.ImageIndex = 1;
+                    break;
+                case 7:
+                    label7.ImageIndex = 1;
+                    break;
+                case 8:
+                    label8.ImageIndex = 1;
+                    break;
+                case 9:
+                    label9.ImageIndex = 1;
+                    break;
+                case 0:
+                    label0.ImageIndex=1;
+                    break;
+            }
+            n += 1;
+            textBox1.Text = n.ToString();
+            dianji = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dianji = true;
+            kaishi:
+            Random r = new Random();
+            int ran = r.Next(0, 10);
+            if (ran == suijishu)
+            {
+                goto kaishi;
+            }
+            switch (ran)
+            {
+                case 1:
+                    label1.ImageIndex = 0;
+                    label1.Visible = true;
+                    break;
+                case 2:
+                    label2.ImageIndex = 0;
+                    label2.Visible = true;
+                    break;
+                case 3:
+                    label3.ImageIndex = 0;
+                    label3.Visible = true;
+                    break;
+                case 4:
+                    label4.ImageIndex = 0;
+                    label4.Visible = true;
+                    break;
+                case 5:
+                    label5.ImageIndex = 0;
+                    label5.Visible = true;
+                    break;
+                case 6:
+                    label6.ImageIndex = 0;
+                    label6.Visible = true;
+                    break;
+                case 7:
+                    label7.ImageIndex = 0;
+                    label7.Visible = true;
+                    break;
+                case 8:
+                    label8.ImageIndex = 0;
+                    label8.Visible = true;
+                    break;
+                case 9:
+                    label9.ImageIndex = 0;
+                    label9.Visible = true;
+                    break;
+                case 0:
+                    label0.ImageIndex = 0;
+                    label0.Visible = true;
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label0.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            suijishu = -1;
+            n = 0;
+            dianji = true;
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            label0.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            string xiaoxi = textBox1.Text;
+            xiaoxi += "分，呵呵！";
+            MessageBox.Show(xiaoxi,"Game Over!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        private void 简单ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            简单ToolStripMenuItem.Checked = true;
+            中等ToolStripMenuItem.Checked = false;
+            困难ToolStripMenuItem.Checked = false;
+            timer1.Interval = 1200;
+            timer2.Interval = 2400;
+        }
+
+        private void 中等ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            简单ToolStripMenuItem.Checked = false;
+            中等ToolStripMenuItem.Checked = true;
+            困难ToolStripMenuItem.Checked = false;
+            timer1.Interval = 1000;
+            timer2.Interval = 2000;
+        }
+
+        private void 困难ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            简单ToolStripMenuItem.Checked = false;
+            中等ToolStripMenuItem.Checked = false;
+            困难ToolStripMenuItem.Checked = true;
+            timer1.Interval = 750;
+            timer2.Interval = 1500;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 帮助ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string output="可以选择3个难度";
+            output+=Environment.NewLine;
+            output+="开始后点击即可敲打地鼠";
+            MessageBox.Show(output, "帮助", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void 关于打地鼠ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nothing want to say", "关于打地鼠", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Text = "Go!";
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.Text = "开始";
+        }
+    }
+}
